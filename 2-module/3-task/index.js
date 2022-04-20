69c3ab0ae7db0747912b9fdc0a2461b69f7bce0e
koroste1 <petro854@gmail.com>
2022-04-20 23:57:43 +0300

    Выполнены задания второго модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,19 @@
 let calculator = {
-  // ваш код
+  firstOperand: 0,
+  secondOperand: 0,
+
+  read(a, b) {
+    this.firstOperand = a;
+    this.secondOperand = b;
+  },
+
+  sum() {
+    return this.firstOperand + this.secondOperand;
+  },
+
+  mul() {
+    return this.firstOperand * this.secondOperand;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
