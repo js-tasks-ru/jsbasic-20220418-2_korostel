7237e4236624c8c5feda7b10f1637a94fb827a44
koroste1 <petro854@gmail.com>
2022-04-27 22:44:56 +0300

    Выполнены задания четвертого модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const ul = document.createElement("ul");
+  ul.innerHTML = friends.reduce((acc, { firstName, lastName }) => {
+    return acc + `<li>${firstName} ${lastName}</li>`;
+  }, "");
+  return ul;
 }
